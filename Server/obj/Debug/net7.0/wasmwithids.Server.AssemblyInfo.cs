1662493bfa25787e72b6aceb9674f720f7487344
dhruv-0987 <dhruvmat1998@gmail.com>
2024-04-02 12:59:17 +1100

    added gitignore

--- Server/obj/Debug/net7.0/wasmwithids.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net7.0/wasmwithids.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wasmwithids.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c464989be165118e8361abe9ac115250b512f2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("wasmwithids.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wasmwithids.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
